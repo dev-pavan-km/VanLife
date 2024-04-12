c17779964cd652ec71c14b7e316bd00e580889d7
dev.pavan.km <dev.pavan.km@gmail.com>
2024-04-12 20:23:43 +0530

    refactor: refactored router provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./About.jsx";
+
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
