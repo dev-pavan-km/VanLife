149894e87576cf3fa58a802f09a26460490a5d53
dev.pavan.km <dev.pavan.km@gmail.com>
2024-04-13 15:56:21 +0530

    feat: display vans list

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import About from "./About.jsx";
-import Home from "./Home.jsx";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import About from "./pages/About.jsx";
+import Home from "./pages/Home.jsx";
+import Vans from "./pages/Vans.jsx";
 
 function App() {
   return (
     <BrowserRouter>
+      <header>
+        <Link to="/" className="site-logo">
+          #VANLIFE
+        </Link>
+        <nav>
+          <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
+        </nav>
+      </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
       </Routes>
     </BrowserRouter>
   );
