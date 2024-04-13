5a6fcf4e212e606342a4d23fa7777052e36b0720
dev.pavan.km <dev.pavan.km@gmail.com>
2024-04-13 17:19:56 +0530

    feat: nested routes with shared layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,20 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./pages/About.jsx";
 import Home from "./pages/Home.jsx";
 import Vans from "./pages/Vans.jsx";
 import VanDetail from "./pages/VanDetail.jsx";
+import Layout from "./components/Layout.jsx";
 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link to="/" className="site-logo">
-          #VANLIFE
-        </Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
