c6383d72cee8abcb4aff1dc90e7ad42cfd46fe84
dev.pavan.km <dev.pavan.km@gmail.com>
2024-04-15 18:40:27 +0530

    feat: host page with nesting and default index route with common layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./pages/About.jsx";
 import Home from "./pages/Home.jsx";
-import Vans from "./pages/Vans.jsx";
-import VanDetail from "./pages/VanDetail.jsx";
+import Vans from "./pages/Vans/Vans.jsx";
+import VanDetail from "./pages/Vans/VanDetail.jsx";
 import Layout from "./components/Layout.jsx";
+import Host from "./pages/Host/Host.jsx";
+import Income from "./pages/Host/Income.jsx";
+import Reviews from "./pages/Host/Reviews.jsx";
+import HostLayout from "./components/HostLayout.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
+
+          <Route path="/host" element={<HostLayout />}>
+            <Route index element={<Host />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,7 @@ export default function Header() {
         #VANLIFE
       </Link>
       <nav>
+        <Link to="/host">Host</Link>
         <Link to="/about">About</Link>
         <Link to="/vans">Vans</Link>
       </nav>
